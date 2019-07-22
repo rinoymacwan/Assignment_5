afb1d891d73dfdc70fa6695c49fc052d77f71597
rinoy <rinoy@promactinfo.com>
2019-07-22 09:19:11 +0530

    done.

--- Assignment_5/Program.cs
+++ Assignment_5/Program.cs
@@ -10,6 +10,20 @@ namespace Assignment_5
     {
         static void Main(string[] args)
         {
+            Dictionary<string, int> prime = new Dictionary<string, int>() { { "Atal Bihari Vajpayee", 1998},
+                { "Narendra Modi", 2014 }, { "Manmohan Singh", 2004 } };
+            Console.WriteLine(prime.FirstOrDefault(x => x.Value==2004).Key + " was the Prime Minister in 2004.");
+
+            prime.Add("Narenda Modi", 2019);
+
+            var sorted = from entry in prime orderby entry.Value ascending select entry;
+
+            foreach (KeyValuePair<string,int> item in sorted)
+            {
+                Console.WriteLine($"{item.Key}:{item.Value}");
+            }
+            Console.ReadKey();
+
         }
     }
 }
